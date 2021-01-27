793b14d2097d3bb2b28e13044c06da43f248edca
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2021-01-26 19:44:57 -0500

    fixed errors

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let Connection;
 if (process.env.JAWSDB_URL) {
   Connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const Connection = mysql.createConnection({
+    Connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
